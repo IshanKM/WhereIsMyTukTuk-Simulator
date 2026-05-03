28991717fb73481cee40282ee0265269e64a24b9
Ishan Madushanka <99119830+IshanKM@users.noreply.github.com>
2026-05-03 11:25:59 +0530

    add new jwt token to access route

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 //export const API_URL = "https://your-live-url/api/v1/locations";
-export const API_URL = "http://localhost:5000/api/v1/locations";
+export const API_URL = "https://api.whereismytuktuk.online/api/v1/locations";
 
-export const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5ZjZiOTQ5ZDA5YjQ0MWJiZTRiNDhlZCIsInJvbGUiOiJvcGVyYXRvciIsImlhdCI6MTc3Nzc3NzAwMCwiZXhwIjoxNzc3ODYzNDAwfQ.mbsmkNvsV6CrBSPSsOfpCvYh4Hufe96hknVf7sk8CuM";
+export const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5ZjZjZDRkMGZkMzc5NzY1NTFiMzhmOSIsInJvbGUiOiJvcGVyYXRvciIsImlhdCI6MTc3Nzc4MjE0MCwiZXhwIjoxNzc3ODY4NTQwfQ.xNe22F_5TY-GrCqkXjW_uefVYSSrBfbbOlAno1El45w";
 
 export const TUKTUKS = [
   "69f5491572f5a84650f1e140",
